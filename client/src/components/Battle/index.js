91828e063fdaf78075c95c38e2e3350f1a0cfb2d
AnnaxGrace <anna.grace.conover@gmail.com>
2020-06-28 09:55:28 -0500

    did skeleton and background for the battles

---
+++ client/src/components/Battle/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./battle.css"
+
+
+function Battle () {
+
+    // function handleNewScreen {
+
+    // }
+    return (
+        // <button type="button" class="btn btn-primary battleBtn" onClick={handleNewScreen}></button>
+    <div id="login-page" class="beige">
+      Hello world!
+      
+    </div>
+    )
+}
+
+export default Battle;
