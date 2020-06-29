324ae5af05b2ebed798b202e75b57ad863fc6373
jose Lopez <vinnielo01@gmail.com>
2020-06-29 12:39:44 -0700

    fixed api routes

--- routes/api/Users.js
+++ routes/api/Users.js
@@ -1,35 +1,10 @@
-let db = require("../../models/User");
-let passport = require("../config/passport");
+const router = require("express").Router();
+const dataController = require("../../controllers/dataController");
 
-module.exports = function (app) {
-  app.post("/api/login", passport.authenticate("local"), function (req, res) {
-    res.json(req.user);
-  });
-
-  app.post("/api/signup", function ({ body }, res) {
-    // console.log(body)
-    db.User.create(body)
-      .then(function () {
-        res.redirect(307, "/api/login");
-      })
-      .catch(function (err) {
-        // console.log(err)
-        res.status(401).json(err);
-      });
-  });
-
-  app.get("/api/signup", function(req,res){
-    db.User.findAll({}).then(function(dbUser) {
-      res.json(dbUser);
-    });
-  })
-
-   // Route for logging user out
-   app.get("/logout", function(req, res) {
-    req.logout();
-    res.redirect("/");
-  });
+// Matches with "/api/signup"
+router.route("/")
+  .get(dataController.findAll)
+  .post(dataController.create);
 
 
-
-};
+  module.exports = router;
