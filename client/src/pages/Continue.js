8339f24f941861a580aa85617faabc003cd8ab80
Julia Clements <tiiedye@gmail.com>
2020-06-30 15:37:36 -0600

    game textbox

--- client/src/pages/Continue.js
+++ client/src/pages/Continue.js
@@ -1,8 +1,39 @@
 import React from "react";
+import TextBox from "../components/TextBox/TextBox"
+import { Container } from "../components/Grid"
+
+const styles={
+    img: {
+        margin: 20
+    }
+}
 
 function Continue() {
     return(
-        <h1>Continue Your Adventure</h1>
+        <Container>
+                <h1 className="text-center">Continue Your Adventure</h1>
+
+                <div>
+                    <img src={require("../images/open-book-board.png")} style={styles.img} />
+
+                    <div>
+                        <TextBox />
+                    </div>
+                </div>
+
+                <div className="card-body text-center">
+                    <p></p>
+                    <h5>Menu</h5>
+
+                    <button className="btn">
+                        Save Game!
+                    </button>
+
+                    <button className="btn">
+                        Text Log
+                    </button>
+                </div>
+        </Container>
     )
 }
 
