31c3fa2cf558c2fc1b34f3e775ffbd2a68b808ad
Julia Clements <tiiedye@gmail.com>
2020-06-30 15:46:38 -0600

    adding more stuff

--- client/src/pages/Continue.js
+++ client/src/pages/Continue.js
@@ -2,19 +2,13 @@ import React from "react";
 import TextBox from "../components/TextBox/TextBox"
 import { Container } from "../components/Grid"
 
-const styles={
-    img: {
-        margin: 20
-    }
-}
-
 function Continue() {
     return(
         <Container>
                 <h1 className="text-center">Continue Your Adventure</h1>
 
                 <div>
-                    <img src={require("../images/open-book-board.png")} style={styles.img} />
+                    <img src={require("../images/open-book-board.png")} />
 
                     <div>
                         <TextBox />
