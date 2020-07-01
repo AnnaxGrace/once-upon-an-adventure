3071f32eb12c68fd160a89a1e55fb3dabbbf7a91
AnnaxGrace <anna.grace.conover@gmail.com>
2020-07-01 14:20:00 -0500

    The snake game runs and adds points. For now, different losses just console log lose, but it will bring up modal

--- client/src/components/CanvasSnake/index.js
+++ client/src/components/CanvasSnake/index.js
@@ -1,46 +1,52 @@
 import React from "react";
 import "./snake.css"
 
+    var xv = 0;
+    var yv = 0;
+    var px = 12;
+    var py = 10;
+    var gs = 20;
+    var tc = 20;
+    var ax = 15;
+    var ay = 15;
+    var wx = 40;
+    var wy = 40;
+
+    var numArray = [1, 2, 3, 4]
+    var trail = [];
+    var tail = 5;
+    var points = 0;
+    var answer = 2;
+    var num1 = 1;
+    var num2 = 1;
+    var run = "Start";
+    let ctx;
+
 class CanvasSnake extends React.Component {
 
     state = {
         number1: 1,
-        number2: 1
+        number2: 1,
+        statePoints: 0
     }
  
     componentDidMount() {
-        var xv = 0;
-        var yv = 0;
-        var px = 12;
-        var py = 10;
-        var gs = 20;
-        var tc = 20;
-        var ax = 15;
-        var ay = 15;
-
-        var numArray = [1, 2, 3, 4]
-        var trail = [];
-        var tail = 5;
-        var points = 0;
-        var answer = 2;
-        var num1 = 1;
-        var num2 = 1;
-        var run = "Start";
+        
 
         let canvas = this.refs.canvas
-        const ctx = canvas.getContext("2d")
+        ctx = canvas.getContext("2d")
 
         let equation = this.refs.equation
         equation.value = num1 + " " + num2 + " = " 
         
         // if ( run === "start") {
-            document.addEventListener("keydown", keyPush);
+            document.addEventListener("keydown", this.keyPush);
             // setInterval(game, 1000/15);
-             setInterval(game, 2000/15);
+             setInterval(this.game, 2000/15);
         
+    }
     
-    
-         function game () {
+    game = () => {
             px += xv;
             py += yv;
             
@@ -78,8 +84,10 @@ class CanvasSnake extends React.Component {
 
                 // if you touch your own tail
                 if(trail[i].x === px && trail[i].y === py) {
-                    // console.log("you lose")
-                    // window.location.reload();
+                    if ( tail > 5) {
+                        console.log("you lose")  
+                    }
+                    
                 }
 
             }
@@ -91,27 +99,43 @@ class CanvasSnake extends React.Component {
                 
             }
 
+            if(wx*gs + (2 * gs * 2) === px*gs && wy*gs === py*gs) {
+                console.log("you LOSE");
+            }
+
             //this is if you touch the right math answer
             if(ax*gs === px*gs && ay*gs === py*gs) {
-                console.log("hi")
+                tail += 4
+                // console.log("hi")
                 ax=Math.floor(Math.random() * tc);
                 ay=Math.floor(Math.random() * tc);
+
+                wx=Math.floor(Math.random() * tc);
+                wy=Math.floor(Math.random() * tc);
+
+                // wx=Math.floor(Math.random() * tc);
+                // wy=Math.floor(Math.random() * tc);
 
                 let indexNum = Math.floor(Math.random() * 4 ) 
                 
                 let indexNum2 = Math.floor(Math.random() * 4 ) 
+
+               
                 
                 num1 = numArray[indexNum]
                 this.setState({number1: num1})
-                console.log(num1)
+                // console.log(num1)
                 num2 = numArray[indexNum2]
                 this.setState({number2: num2})
                 console.log(num2)
 
                 answer = num1 + num2
+                points++;
+                this.setState({statePoints: points})
+                console.log(points)
+                console.log("answer")
+                console.log(answer)
                 
-                
-
             }
 
             
@@ -120,15 +144,15 @@ class CanvasSnake extends React.Component {
             
             //right answers
             ctx.fillText(answer, ax*gs, (ay*gs) + 20 )
-
-            //This will be the wrong answers
-            // for (i = 0; i < points.length; i++) {
-            //     ctx.fillText(answer + ranNum, ax*ga*ranNum, (ay*gs*ranNum) + 20 )
-            // }
+            
+            //wrong answer
+           let ranNum = 2
+                
+            ctx.fillText(answer + ranNum, wx*gs + (2 * gs * 2), (wy*gs) + 20 )
         
     }
 
-     function keyPush (event)  {
+     keyPush = event =>  {
         switch(event.keyCode) {
             case 37:
                 xv = -1;
@@ -152,14 +176,15 @@ class CanvasSnake extends React.Component {
 
     }
 
-}
+
 
     render() {
         return(
           <div>
             <canvas ref="canvas" width={500} height={400} />
                 <div ref="equation" id="equation">
-                    {this.state.number1} + {this.state.number2} =
+                    <p>{this.state.number1} + {this.state.number2} = </p>
+                    <p>Points: {this.state.statePoints} </p>
                 </div>
           </div>
         )
