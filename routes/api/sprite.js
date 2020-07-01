b88d26af18a76568e5a0f161a9217bc336ba3e23
jose Lopez <vinnielo01@gmail.com>
2020-07-01 13:51:18 -0700

    added api's an conrtollers for inventory and sprites

---
+++ routes/api/sprite.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+
+
+// // Matches with "/api/sprite"
+// router.route("/")
+//   .get(spriteController.findAll)
+//   .post(spriteController.create);
+
+// // Matches with "/api/books/:id"
+// router
+//   .route("/:id")
+//   .get(spriteController.findById)
+//   .put(spriteController.update)
+//   .delete(spriteController.remove);
+
+// module.exports = router;
