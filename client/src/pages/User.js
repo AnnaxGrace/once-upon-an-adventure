94dec6fbd03d0f1c3022f077822bd31d2fc271a5
Julia Clements <tiiedye@gmail.com>
2020-07-02 12:27:17 -0600

    designing page

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -1,37 +1,62 @@
 import React from "react";
 import { Container } from "../components/Grid";
 import { Link } from "react-router-dom";
+import UserSplash from "../components/UserSplash/UserSplash";
+
+const styles={
+    bookImg: {
+        width: 200,
+        marginTop: 10
+    }
+}
 
 function User() {
     return(
         <Container>
-            <div className="text-center">
-                <h1>Welcome to Your Realm</h1>
+                <h1 className="text-center">Welcome to Your Realm</h1>
 
-                <p>
-                    (maybe insert random sprite here!)
-                </p>
+                <div className="row">
+                    <div className="col-md-6">
+                        <UserSplash />
+                    </div>
 
-                <p>
-                    <Link to="/continue">
-                        <button className="btn">
-                            Continue your Adventure!
-                        </button>
-                    </Link>
-                    <br />
-                    <Link to="/new">
-                        <button className="btn">
-                            Start a New Adventure!
-                        </button>
-                    </Link>
-                    <br />
-                    <Link to="/">
-                        <button className="btn">
-                            Log Out
-                        </button>
-                    </Link>
-                </p>
-            </div>
+                    <div className="col-md-6">
+                        <div className="card text-center">
+                            <div className="card-body special">
+                                <h3>Your Menu</h3>
+                                <img src={require("../images/Book-Pile.png")} alt="book pile" style={styles.bookImg} />
+
+                                <p>
+                                    React RPG (Title Pending) is an interactive 
+                                    learning game. From this menu you can continue 
+                                    a previous Adventure (if you have one saved), 
+                                    or start a new Adventure. Enjoy your time exploring 
+                                    the word we've created for you!
+                                </p>
+
+                                <Link to="/continue">
+                                    <button className="btn">
+                                        Continue your Adventure!
+                                    </button>
+                                </Link>
+                                <br />
+
+                                <Link to="/new">
+                                    <button className="btn">
+                                        Start a New Adventure!
+                                    </button>
+                                </Link>
+                                <br />
+
+                                <Link to="/">
+                                    <button className="btn">
+                                        Log Out
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </Container>
     )
 }
