5a12594c37fe1d13a37a6712a3068f77d05ae416
Julia Clements <tiiedye@gmail.com>
2020-07-03 10:47:29 -0600

    adding additional credits

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -1,6 +1,7 @@
 import React from "react";
 // import {canvas, xAxis, yAxis, srcX, srcY, width, height, context, curretFrame, selectedChar, character, animate, updateFrame, drawSprite} from "../assets/teamMovement/animate";
 // import useAnimation from "../assets/teamMovement/animate"
+import { Container } from "../components/Grid"
 
 const styles = {
     sprites: {
@@ -17,6 +18,9 @@ const styles = {
         width: 150,
         height: 250,
         margin: "auto"
+    },
+    creditsDiv: {
+        marginTop: 50
     }
 }
 
@@ -59,20 +63,35 @@ const teamInfo = [
 
 function Team() {
     return(
-        <div style={styles.divPos}>
-            <h1 className="text-center">The Team</h1>
-                <div className="row">
-                    {teamInfo.map(item => (
-                        <div className="col-md-2 card text-center" style={styles.devCard} key={item.id}>
-                            {/* <canvas id="canvas" style={styles.teamImg}></canvas> */}
-                            <img src={item.img} style={styles.teamImg} alt={item.name} />
-                                <h5>{item.name}</h5>
-                                    <p className="special">
-                                        {item.roles}
-                                    </p>
-                        </div>
-                    ))}
+        <div>
+            <div style={styles.divPos}>
+                <h1 className="text-center">The Team</h1>
+                    <div className="row">
+                        {teamInfo.map(item => (
+                            <div className="col-md-2 card text-center" style={styles.devCard} key={item.id}>
+                                {/* <canvas id="canvas" style={styles.teamImg}></canvas> */}
+                                <img src={item.img} style={styles.teamImg} alt={item.name} />
+                                    <h5>{item.name}</h5>
+                                        <p className="special">
+                                            {item.roles}
+                                        </p>
+                            </div>
+                        ))}
+                    </div>
+            </div>
+
+            <Container>
+                <div className="text-center" style={styles.creditsDiv}>
+                    <h5>Additional Credits</h5>
+                    <p>
+                        Images and Sprites sourced from:
+                        <br />
+                        <a href="https://www.freevector.com/" target="_blank" rel="noopener noreferrer">FreeVector.com</a>
+                        <br />
+                        <a href="https://kronbits.itch.io/matriax-free-assets" target="_blank" rel="noopener noreferrer">Kronbits</a>
+                    </p>
                 </div>
+            </Container>
         </div>
     )
 }
