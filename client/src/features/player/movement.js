aa08b071e302a69ebb0dbd4fd0eb6ab95f64bfc7
erik hirsch <ehirsch760@gmail.com>
2020-07-04 00:23:09 -0700

    tile logic layout

--- client/src/features/player/movement.js
+++ client/src/features/player/movement.js
@@ -1,104 +1,142 @@
 import store from '../../config/store'
 import { SPRITE_SIZE, SPRITE_SHEET_HEIGHT, SPRITE_SHEET_WIDTH, MAP_WIDTH, MAP_HEIGHT, HALF_GRID } from '../../config/constants'
-import walking from '../sound/index'
+import { walkingStone, walkingGrass, walkingGravel, ouch} from '../sound/index'
 
-    function getNewPosition(oldPos, direction) {
-        switch (direction) {
-            case 'WEST':
-                walking.play();
-                return [oldPos[0] - SPRITE_SIZE, oldPos[1]]
-            case 'EAST':
-                walking.play();
-                return [oldPos[0] + SPRITE_SIZE, oldPos[1]]
-            case 'NORTH':
-                walking.play();
-                return [oldPos[0], oldPos[1] - SPRITE_SIZE]
-            case 'SOUTH':
-                walking.play();
-                return [oldPos[0], oldPos[1] + SPRITE_SIZE]
-        }
+
+function getNewPosition(oldPos, direction) {
+    switch (direction) {
+        case 'WEST':
+            return [oldPos[0] - SPRITE_SIZE, oldPos[1]]
+        case 'EAST':
+            return [oldPos[0] + SPRITE_SIZE, oldPos[1]]
+        case 'NORTH':
+            return [oldPos[0], oldPos[1] - SPRITE_SIZE]
+        case 'SOUTH':
+            return [oldPos[0], oldPos[1] + SPRITE_SIZE]
     }
+}
 
 
-    function getSpriteLocation(direction, walkIndex) {
+function getSpriteLocation(direction, walkIndex) {
 
-        switch (direction) {
-            case 'WEST':
-                //rows are selected from bottom up...
-                return `${SPRITE_SIZE * walkIndex}px ${SPRITE_SIZE * 3}px` //768px
-            case 'EAST':
-                return `${SPRITE_SIZE * walkIndex}px ${SPRITE_SIZE * 1}px`//640px
-            case 'NORTH': ;
-                return `${SPRITE_SIZE * walkIndex}px ${SPRITE_SIZE * 0}px`//576px
-            case 'SOUTH':
-                return `${SPRITE_SIZE * walkIndex}px ${SPRITE_SIZE * 2}px`//704px
-        }
+    switch (direction) {
+        case 'WEST':
+            //rows are selected from bottom up...
+            return `${SPRITE_SIZE * walkIndex}px ${SPRITE_SIZE * 3}px` //768px
+        case 'EAST':
+            return `${SPRITE_SIZE * walkIndex}px ${SPRITE_SIZE * 1}px`//640px
+        case 'NORTH': ;
+            return `${SPRITE_SIZE * walkIndex}px ${SPRITE_SIZE * 0}px`//576px
+        case 'SOUTH':
+            return `${SPRITE_SIZE * walkIndex}px ${SPRITE_SIZE * 2}px`//704px
     }
+}
 
-    function getWalkIndex() {
-        const walkIndex = store.getState().player.walkIndex
+function getWalkIndex() {
+    const walkIndex = store.getState().player.walkIndex
 
-        return walkIndex >= 8 ? 0 : walkIndex + 1
-    }
+    return walkIndex >= 8 ? 0 : walkIndex + 1
+}
 
-    function observeBoundaries(oldPos, newPos) {
-        return (newPos[0] >= 0 && newPos[0] <= MAP_WIDTH - SPRITE_SIZE) &&
-            (newPos[1] >= 0 && newPos[1] <= MAP_HEIGHT - SPRITE_SIZE)
-    }
+function observeBoundaries(oldPos, newPos) {
+    return (newPos[0] >= 0 && newPos[0] <= MAP_WIDTH - SPRITE_SIZE) &&
+        (newPos[1] >= 0 && newPos[1] <= MAP_HEIGHT - SPRITE_SIZE)
+}
 
-    function observeImpassable(oldPos, newPos) {
-        const tiles = store.getState().map.tiles
-        const y = newPos[1] / SPRITE_SIZE
-        const x = newPos[0] / SPRITE_SIZE
-        const nextTile = tiles[y][x]
-        console.log(nextTile)
-        console.log("characters position",newPos)
-        return nextTile < 10
-    }
+function observeImpassable(oldPos, newPos) {
+    const tiles = store.getState().map.tiles
+    const y = newPos[1] / SPRITE_SIZE
+    const x = newPos[0] / SPRITE_SIZE
+    const nextTile = tiles[y][x]
+    console.log(nextTile)
+    console.log("characters position", newPos)
+    switch (nextTile) {
+        case 0:  //Cliff Edge
 
-    function dispatchMove(direction, newPos) {
-        const walkIndex = getWalkIndex()
+            break;
+        case 1: //Cliff Body
 
-        store.dispatch({
-            type: 'MOVE_PLAYER',
-            payload: {
-                position: newPos,
-                direction,
-                walkIndex,
-                spriteLocation: getSpriteLocation(direction, walkIndex),
-            }
+            break;
+        case 2:  //Cliff Edge Edit?
 
-        })
-    }
+            break;
+        case 3:  //Sky
 
-    function attemptMove(direction) {
-        const oldPos = store.getState().player.position
-        const newPos = getNewPosition(oldPos, direction)
-        console.log(observeBoundaries(oldPos, newPos))
-        if (observeBoundaries(oldPos, newPos) && observeImpassable(oldPos, newPos))
-            dispatchMove(direction, newPos)
-    }
+            break;
+        case 4:  //grass
+        walkingGrass.play();
+            break;
+        case 5:  //dirt
+        walkingGravel.play();
+            break;
+        case 6:  //stone
+        walkingStone.play();
+            break;
+        case 7:  //fake Tree
 
+            break;
+        case 8:  //return to book
 
-    function handleKeyDown(e) {
-        e.preventDefault()
-        switch (e.keyCode) {
-            case 37:
-                return attemptMove('WEST');
+            break;
+        case 9:  //talk to Jace
 
-            case 38:
-                return attemptMove('NORTH');
-
-            case 39:
-                return attemptMove('EAST');
-
+            break;
             case 40:
-                return attemptMove('SOUTH');
+                ouch.play()
+                break;
 
-            default:
-                console.log(e.keyCode)
-        }
     }
 
+    if (nextTile > 32) {
+        console.log("ouch")
+      ouch.play() 
+    }
 
-    export { getNewPosition, getWalkIndex, getSpriteLocation, observeBoundaries, observeImpassable, dispatchMove, attemptMove, handleKeyDown }
+    return nextTile < 32
+}
+
+function dispatchMove(direction, newPos) {
+    const walkIndex = getWalkIndex()
+    store.dispatch({
+        type: 'MOVE_PLAYER',
+        payload: {
+            position: newPos,
+            direction,
+            walkIndex,
+            spriteLocation: getSpriteLocation(direction, walkIndex),
+        }
+
+    })
+}
+
+function attemptMove(direction) {
+    const oldPos = store.getState().player.position
+    const newPos = getNewPosition(oldPos, direction)
+    console.log(observeBoundaries(oldPos, newPos))
+    if (observeBoundaries(oldPos, newPos) && observeImpassable(oldPos, newPos))
+        dispatchMove(direction, newPos)
+}
+
+
+function handleKeyDown(e) {
+    e.preventDefault()
+    switch (e.keyCode) {
+        case 37:
+            return attemptMove('WEST');
+
+        case 38:
+            return attemptMove('NORTH');
+
+        case 39:
+            return attemptMove('EAST');
+
+        case 40:
+            return attemptMove('SOUTH');
+
+        default:
+            console.log(e.keyCode)
+    }
+}
+
+
+export { getNewPosition, getWalkIndex, getSpriteLocation, observeBoundaries, observeImpassable, dispatchMove, attemptMove, handleKeyDown }
--- client/src/features/sound/index.js
+++ client/src/features/sound/index.js
@@ -1,11 +1,31 @@
-import {Howl, Howler} from 'howler';
+import { Howl } from 'howler';
 
-const walking = new Howl({
+const walkingStone = new Howl({
   src: 'https://audio-previews.elements.envatousercontent.com/files/292851973/preview.mp3',
   html5: true,
   format:["mp3", "aac"],
   volume: 0.25
 });
   
+const walkingGrass = new Howl({
+  src: 'https://raw.githubusercontent.com/AnnaxGrace/react-rpg/master/client/src/features/sound/footstep.mp3',
+  html5: true,
+  format:["mp3", "aac"],
+  volume: 0.25
+});
 
-export default walking;
+const walkingGravel = new Howl({
+  src: 'https://audio-previews.elements.envatousercontent.com/files/282624049/preview.mp3',
+  html5: true,
+  format:["mp3", "aac"],
+  volume: 0.25
+});
+
+const ouch = new Howl({
+  src: 'https://audio-previews.elements.envatousercontent.com/files/111423304/preview.mp3',
+  html5: true,
+  format:["mp3", "aac"],
+  volume: 0.25
+});
+
+export { walkingStone, walkingGrass, ouch, walkingGravel};
