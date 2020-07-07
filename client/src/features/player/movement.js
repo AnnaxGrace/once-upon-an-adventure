8d8c0807c122f4e2b984f8d2046226a8171d7c2b
erik hirsch <ehirsch760@gmail.com>
2020-07-07 11:51:08 -0700

    fixed error

--- client/src/features/player/movement.js
+++ client/src/features/player/movement.js
@@ -1,6 +1,6 @@
 import store from '../../config/store'
 import { SPRITE_SIZE,  MAP_WIDTH, MAP_HEIGHT } from '../../config/constants'
-import { walkingStone, walkingGrass, walkingGravel, impact1, impact2, rustlingFoliage, orcBabble, guardTalk } from '../sound/index'
+import { walkingStone, walkingGrass, walkingGravel, impact1, impact2, rustlingFoliage, orcBabble, guardTalk, magicalJace } from '../sound/index'
 // import GameTextBox from "../../components/TextBox/GameTextBox"
 
 
