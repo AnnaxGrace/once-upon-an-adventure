eaa43759312a28f41cb7586e2f51d024fca9ebe7
AlkaTone <antonio.a.garces@gmail.com>
2020-07-15 14:39:22 -0700

    added anna to forest

--- client/src/features/map/index.js
+++ client/src/features/map/index.js
@@ -153,6 +153,8 @@ function getTileSprite(type) {
                 return 'erikShop18'
             case 249:
                 return 'erikShop19'
+            case 250:
+                return 'thief'
 
         // Forest
         case 33:
