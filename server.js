f29c3216de14cd2ebeeeda9374c532a08140b1f0
AnnaxGrace <anna.grace.conover@gmail.com>
2020-08-25 12:46:16 -0500

    Added new path for mongoAtlas vs MLab

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
+
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactrpgX");
+mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/reactrpgX");
 
 // Start the API server
 app.listen(PORT, function() {
